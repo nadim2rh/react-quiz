47d41886a09a5d7e918c66fb8a46a14dd752674e
Nadim Rahman <rhmnnadim@gmail.com>
2022-05-07 22:43:25 +0800

    Routing updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "../context/AuthContext";
 import "../styles/App.css";
 import Layout from "./Layout";
 import Home from "./pages/Home";
-import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "../context/AuthContext";
+import Signup from "./pages/Signup";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 function App() {
   return (
@@ -15,10 +17,14 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/result" element={<Result />} />
+            <Route path="/*" element={<PublicRoute />}>
+              <Route path="signup" element={<Signup />} />
+              <Route path="login" element={<Login />} />
+            </Route>
+            <Route path="/*" element={<PrivateRoute />}>
+              <Route path="quiz" element={<Quiz />} />
+              <Route path="result" element={<Result />} />
+            </Route>
           </Routes>
         </Layout>
       </AuthProvider>
